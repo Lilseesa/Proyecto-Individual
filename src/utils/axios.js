2fd2914e4a5eeecb10302fc19c1f3ac4d4393c2d
Sven Seemann <svenebal@gmail.com>
2020-06-27 11:22:46 -0600

    final

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'https://api.github.com';
 
 axios.defaults.headers = {
     Accept: 'application/vnd.github.v3+json',
-    Authorization: 'Token 01608af3d2331902798bcb3277c645197e2c6f7e',
+    Authorization: 'Token ',
 };
 
 export default axios;
