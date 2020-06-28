18de31d4f6fb2fff8025f44d25da75915d8f7e72
Sven Seemann <svenebal@gmail.com>
2020-06-27 23:38:10 -0600

    Funciona, listo para hostear

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+//const NODE_ENV = process.env.NODE_ENV || 'development';
+
 axios.defaults.baseURL = 'https://api.github.com';
+
+console.log(process.env);
 
 axios.defaults.headers = {
     Accept: 'application/vnd.github.v3+json',
-    Authorization: 'Token ',
+    Authorization: process.env.REACT_APP_KEY,
 };
 
 export default axios;
